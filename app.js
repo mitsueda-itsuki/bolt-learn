29801b26b1b502bab8da515f02b9e8a9ccda2ec9
mitsueda <mitsueda.itsuki.bc@gmail.com>
2021-05-10 22:45:27 +0900

    ぬるぽ => ガッ

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const { App } = require('@slack/bolt');
 const { config } = require('dotenv');
 config();
 
+const { null_po } = require('./scripts/null_po')
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
@@ -10,6 +12,8 @@ const app = new App({
 app.message('hello', async ({ message, say }) => {
   await say(`Hello <@${message.user}>!`);
 });
+
+app.message('ぬるぽ', null_po);
 
 (async () => {
   await app.start(process.env.PORT || 3000);
---
+++ scripts/null_po.js
@@ -0,0 +1,14 @@
+const GA =`
+　　 （　・∀・）　　　|　|　ｶﾞｯ
+　　と　　　　）　 　 |　|
+　　　 Ｙ　/ノ　　　 人
+　　　　 /　）　 　 < 　>__Λ∩
+　　 ＿/し'　／／. Ｖ｀Д´）/ ←>>1
+　　（＿フ彡　　　　　 　　/
+`;
+
+const null_po = async ({ say }) => {
+  await say(GA)
+};
+
+exports.null_po = null_po;
